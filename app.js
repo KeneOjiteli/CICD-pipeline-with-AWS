71f3c37a747ead3b8d8a5db9d9d62d86dfad30f8
KeneOjiteli <ojitelikenechukwu@gmail.com>
2023-04-11 11:35:29 +0100

    Implementing Manual Review Feature

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Building a CICD pipeline with AWS build stacks like AWS Code Pipeline and Code Build'));
+app.get('/', (req, res) => res.send('Project: Building a CICD pipeline with AWS build stacks like AWS Code Pipeline and Code Build'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
